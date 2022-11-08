868a3cadd8933d2b492b6099d54cb401d68b1e55
Raphael00000 <mongrolle.raphael@gmail.com>
2022-11-08 10:28:10 +0100

    first

---
+++ accueil.php
@@ -0,0 +1,46 @@
+
+
+<center>
+<h1> Design 4 Green </h1>
+</center>
+
+
+<script>
+var myIndex = 0;
+carousel();
+
+/*
+function plusDivs(n) {
+    var x = document.getElementsByClassName("mySlides");
+    var y = document.getElementsByClassName("captionSlide");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+        y[i].style.display = "none";  
+    }
+    if(n==-1 && myIndex==1){
+       myIndex= x.length;
+    }else if(n==1 && myIndex==x.length){
+       myIndex= 1;
+    }else{
+       myIndex+= n;
+    }
+    x[myIndex-1].style.display = "block";  
+    y[myIndex-1].style.display = "block";  
+}
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("captionSlide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    y[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  y[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 15000); // Change image every 2 seconds
+}
+*/
+</script>
