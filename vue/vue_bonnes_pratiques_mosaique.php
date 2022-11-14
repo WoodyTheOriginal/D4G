b4e75b51b492a176a72ae4f37deafd9f3ac3304e
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2022-11-14 08:39:04 +0100

    fdfds

--- vue/vue_bonnes_pratiques_mosaique.php
+++ vue/vue_bonnes_pratiques_mosaique.php
@@ -2,23 +2,20 @@
 	<h1>Les Bonnes pratiques</h1>
 </center>
 <?php
-	echo "<div class ='container_box'>";
+	echo "<div class='container_box'>";
 		foreach ($lesPratiques as $unePratique) {
 			echo "<div class ='box'>
-					<div class ='boxDate'>";
+					<div class='boxDate'>";
 			echo $unePratique['Famille_Origine'];
 			echo "</div>";
-			echo "<div class ='boxDetail'>";
 			echo $unePratique['CRITERES'];
-			echo "</div>";
 			if ($unePratique['INCONTOURNABLE'] == 'INCONTOURNABLE') {
 				echo "<input type='checkbox' checked/>";
 			}
 			else {
 				echo "<input type='checkbox'/>";
 			}			
-			echo "</div>
-				</div>";
+			echo "</div>";
 
 		}
 	echo "</div>";
