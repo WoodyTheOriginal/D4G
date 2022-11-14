ed2197049e3a9691d83d0056b019f863daa02ca3
WoodyTheOriginal <mathieu1.guignard@gmail.com>
2022-11-14 19:09:46 +0100

    pratiques propre

--- vue/vue_mosaique_architecture.php
+++ vue/vue_mosaique_architecture.php
@@ -7,18 +7,16 @@ foreach ($lesPratiques as $unePratique) {
 
 	echo "<div class ='card'>
 			<div class='card-body'>
-			<div class ='card-title'>";
-			echo $unePratique['FamilleOrigine'];
-	echo 	"</div>";
-	echo "<div class ='card-text'>";
-	echo $unePratique['CRITERES'];
-	echo "</div>";
-	echo "<a href='index.php?page=1&idpratique=" . $unePratique['ID'] . "'><div class ='boxInfo'>
-		<div class='btn btn-primary'<button type='submit'>Ajouter</button></div></a>";
-	echo "<a href='#'><div class ='boxInf'>";
-	echo "</div></a>
-						</div>
-					</div>";
+				<div class ='card-title'>";
+				echo $unePratique['FamilleOrigine'];
+				echo "</div>";
+				echo "<div class ='card-text'>";
+				echo $unePratique['CRITERES'];
+				echo "</div>";
+				echo "<a href='index.php?page=1&idpratique=" . $unePratique['ID'] . "'><div class='btn btn-primary'<button type='submit'>Ajouter</button></div></a>";
+				echo "<a href='#'><div class ='boxInf'></div></a>
+			</div>
+		</div>";
 
 }
 echo "</div>";
--- vue/vue_mosaique_ux-ui.php
+++ vue/vue_mosaique_ux-ui.php
@@ -1,23 +1,21 @@
 <center><h1>UX/UI</h1></center>
 <?php
-	echo "<div class ='container'>";
-	foreach ($lesPratiques as $unePratique) {
+echo "<div class ='container'>";
+foreach ($lesPratiques as $unePratique) {
 
-		echo "<div class ='card'>
-				<div class='card-body'>
+	echo "<div class ='card'>
+			<div class='card-body'>
 				<div class ='card-title'>";
 				echo $unePratique['FamilleOrigine'];
-		echo 	"</div>";
-		echo "<div class ='card-text'>";
-		echo $unePratique['CRITERES'];
-		echo "</div>";
-		echo "<a href='index.php?page=1&idpratique=" . $unePratique['ID'] . "'><div class ='boxInfo'>
-			<div class='btn btn-primary'<button type='submit'>Ajouter</button></div></a>";
-		echo "<a href='#'><div class ='boxInf'>";
-		echo "</div></a>
-							</div>
-						</div>";
-	
-	}
-	echo"</div>";
-	?>
+				echo "</div>";
+				echo "<div class ='card-text'>";
+				echo $unePratique['CRITERES'];
+				echo "</div>";
+				echo "<a href='index.php?page=1&idpratique=" . $unePratique['ID'] . "'><div class='btn btn-primary'<button type='submit'>Ajouter</button></div></a>";
+				echo "<a href='#'><div class ='boxInf'></div></a>
+			</div>
+		</div>";
+
+}
+echo "</div>";
+?>
